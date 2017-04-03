9e4f4819e4bfeacee45959b2bb38f795d5a77ccd
sebastiaan <sebastiaanvoebal@hotmail.com>
2017-04-03 09:32:47 +0200

    ik weet niet wat dit is

---
+++ hombreGato/Assets/Spawning_Object.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawning_Object: MonoBehaviour
+{
+    [SerializeField]
+    private GameObject objectToSpawn;
+
+    private Vector3 spawnPosition;
+    private float spawnDelay = 1f;
+    private float speed = 0f;
+    void Start()
+    {
+        spawnPosition = new Vector3(transform.position.x, speed, transform.position.z);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Spawn();
+        }
+    }
+
+    void Spawn()
+    {
+        GameObject e = Instantiate(objectToSpawn, spawnPosition, Quaternion.identity) as GameObject;
+        e.transform.SetParent(transform);
+    }
+}
---
+++ hombreGato/Assets/SpinningGround.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+  
+
+public class SpinningGround : MonoBehaviour {
+    [SerializeField]
+    private float rotateSpeed = 100f;
+    [SerializeField]
+    private bool rotateX = false;
+
+    [SerializeField]
+    private bool rotateY = false;
+
+    [SerializeField]
+    private bool rotateZ = false;
+
+    [SerializeField]
+    private bool possitiveRotation = true;
+
+    [SerializeField]
+    private int posOrNeg = 1;
+
+
+
+    void Start () {
+		if(possitiveRotation == false)
+        {
+
+            posOrNeg = -1;
+        }
+	}
+	
+	void Update () {
+        if (rotateX)
+        {
+            transform.Rotate(rotateSpeed * Time.deltaTime * posOrNeg, 0, 0);
+
+        }
+        if (rotateY)
+        {
+
+            transform.Rotate(0, rotateSpeed * Time.deltaTime * posOrNeg,0);
+
+        }
+        if (rotateZ)
+        {
+
+            transform.Rotate(0, 0, rotateSpeed * Time.deltaTime * posOrNeg);
+        }
+	}
+}
