019a72200155ce769953857eb3381789cb579569
sebastiaan <sebastiaanvoebal@hotmail.com>
2017-04-03 10:15:33 +0200

    Baloon movemnt

---
+++ hombreGato/Assets/scripts/BaloonMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaloonMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed = 10f;
+    [SerializeField]
+    private Rigidbody rb;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+
+
+        Vector3 movement = new Vector3(0f, 4F, 0f);
+
+        rb.velocity = movement * movementSpeed;
+    }   
+}
+    
